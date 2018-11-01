f2eb91bd41dd05aac885eb4f14603be635cebad1
Ivy-IV <protox@ntlworld.com>
2018-11-01 16:17:17 +0000

    can now add files individually

--- gui.py
+++ gui.py
@@ -26,19 +26,21 @@ def scanWindow():
     scan = Toplevel(root)
     scan.title("Scan Folder")
     scan.geometry("400x600")
-    scan.grid_columnconfigure(index=0, weight=1)
     scan.grid_columnconfigure(index=1, weight=1)
     scan.grab_set()
 
-    def getList():
-        path = filedialog.askdirectory()
-        games = search(path)
-        for item in games:
-            if Path(item) not in [Path(i) for i in addList.get(0, END)]: addList.insert(END, item)
-        addList.grid(columnspan=2, row=2, sticky=EW+NS,)
-        scrolly.grid(column=2, row=2, sticky=NS)
-        scrollx.grid(columnspan=2, row=3, sticky=EW)
-        addButton.grid(column=1, row=5)
+    def getList(type):
+        # Add single file to list
+        if type == 0:
+            path = filedialog.askopenfilename()
+            if Path(path) not in [Path(i) for i in addList.get(0, END)]: addList.insert(END, path)
+        # Scan folder and add contents to list
+        elif type == 1:
+            path = filedialog.askdirectory()
+            games = search(path)
+            for item in games:
+                if Path(item) not in [Path(i) for i in addList.get(0, END)]: addList.insert(END, item)
+        addButton.grid(column=5, row=5, sticky=SE)
 
     # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!TODO!!!!!!!!!!!!!
     def addGame():
@@ -57,14 +59,20 @@ def scanWindow():
 
     # !!!LIST!!!
     scrolly = Scrollbar(scan)
+    scrolly.grid(column=3, rowspan=2, sticky=NS)
     scrollx = Scrollbar(scan, orient=HORIZONTAL)
+    scrollx.grid(column=1, columnspan=2, row=2, sticky=EW)
     addList = Listbox(scan, selectmode=MULTIPLE,
     yscrollcommand=scrolly.set, xscrollcommand=scrollx.set)
+    addList.grid(column=1, columnspan=2, row=0, rowspan = 2, sticky=EW+NS,)
     scrolly.config(command=addList.yview)
     scrollx.config(command=addList.xview)
     # !!!BUTTONS!!!
-    pathButton = Button(scan, text="Folder", command=getList)
-    pathButton.grid(column=0, row=0)
+    fileButton = Button(scan, text="Select File", command=lambda: getList(0))
+    fileButton.grid(column=0, row=0, sticky=N+EW)
+
+    folderButton = Button(scan, text="Scan Folder", command=lambda: getList(1))
+    folderButton.grid(column=0, row=1, sticky=N+EW)
 
     addButton = Button(scan, text="Add to Games", command=addGame)
 
